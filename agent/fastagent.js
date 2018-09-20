0639c021769547824f6873f9cb8a11ec16afa004
Tiago Prosp <tiago@prosp>
2018-09-20 19:12:36 +0100

    new faster approach

---
+++ agent/fastagent.js
@@ -0,0 +1,116 @@
+const tf = require('@tensorflow/tfjs')
+const _ = require('lodash')
+
+// Load the binding:
+//require('@tensorflow/tfjs-node')
+
+class Agent{
+    constructor(stateSize, is_eval = false, modelName = '') {
+        this.stateSize = stateSize
+        this.actionSize = 3
+        this.memory = []
+        this.inventory = []
+        this.modelName = modelName
+        this.eval = is_eval
+
+        this.gamma = 0.95
+        this.epsilon = 1.0
+        this.epsilonMin = 0.01
+        this.epsilonDecay = 0.995
+
+        this.model = is_eval ? tf.loadModel('models/' + modelName) : this._model()
+    }
+
+    _model() {
+        const model = tf.sequential()
+        model.add(tf.layers.dense({
+            units: 64,
+            inputShape: [this.stateSize],
+            //inputDim:  this.stateSize,
+            activation: 'relu'
+        }))
+        model.add(tf.layers.dense({units: 32, activation: 'relu'}))
+        model.add(tf.layers.dense({units: 8, activation: 'relu'}))
+        model.add(tf.layers.dense({units: this.actionSize, activation: 'linear'}))
+        model.compile({
+            optimizer: tf.train.adam(0.000001),
+            loss: 'categoricalCrossentropy',
+            metrics: ['accuracy']
+        })
+
+        return model
+    }
+
+    action(state) {
+        return tf.tidy(() => {
+            if(!this.eval && Math.random() <= this.epsilon){
+                return _.random(this.actionSize)
+            }
+
+            let _state = tf.tensor(state).reshape([-1, this.stateSize])
+            let options = this.model.predict(_state)
+            options.print()
+            return tf.argMax(options).dataSync()[0]
+        })
+    }
+
+    async expReplay(batchSize) {
+
+        let X = []
+        let Y = []
+                
+        await this.memory.map((mem, i) => {
+            
+            let [state, action, reward, next_state, done] = mem
+            let target = reward
+            
+            let _state = tf.tensor(state).reshape([-1, this.stateSize])
+            let _next_state = tf.tensor(next_state).reshape([-1, this.stateSize])
+
+            if (!done) {
+                let predictNext = this.model.predict(_next_state)
+                target = reward + this.gamma * predictNext.argMax().dataSync()[0]
+                //console.log(predictNext.dataSync())
+            }
+
+            let target_f = this.model.predict(_state).dataSync()
+            target_f[action] = target
+
+            _state.dispose()
+            _next_state.dispose()
+
+            X.push(state)
+            Y.push(Array.from(target_f))
+        })
+
+        X = tf.tensor(X).reshape([-1, this.stateSize])
+        Y = tf.tensor2d(Y)
+
+        await this.model.fit(X, Y, {
+            epochs: 1,
+            batchSize,
+            // validationSplit: 0.15,
+            // shuffle: true,
+            callbacks: {
+                // onBatchEnd: (batch, logs) => {
+                //     console.log(`${logs.loss} ${logs.acc}`)
+                // },
+                // onEpochEnd: (epoch, logs) => {
+                //     console.log(`${logs.val_loss} ${logs.val_acc}`)
+                // }
+            }
+        })
+        
+        //await this.model.predict(X).print()
+
+        if (this.epsilon > this.epsilonMin) {
+            this.epsilon *= this.epsilonDecay
+        }
+
+        X.dispose()
+        Y.dispose()
+
+    }
+}
+
+module.exports = Agent
---
+++ fasttrain.js
@@ -0,0 +1,78 @@
+const args = process.argv
+const _ = require('lodash')
+const {getData, getState} = require('./utils')
+
+const Agent = require('./agent/fastagent')
+console.log('Start script with: node --max-old-space-size=4096')
+
+if(args.length !== 5){
+    console.log('Usage: node train [PAIR] [WINDOW] [EPISODES]')
+    process.exit(-1)
+}
+
+let [pair_name, window_size, episode_count] = args.slice(2)
+window_size = +window_size
+episode_count = +episode_count
+
+let agent = new Agent(window_size)
+let data = getData(pair_name)
+let l = data.length - 1
+let batch_size = 32
+const MAX_MEM = 1500
+
+const train = async () => {
+
+    for (let e = 0; e < episode_count + 1; e++) {
+        console.log('Episode ' + e + '/' + episode_count)
+        let state = getState(data, 0, window_size + 1)
+        
+        total_profit = 0
+        agent.inventory = []
+        total_trades = 0
+
+        for (let t = 0; t < l; t++) {
+            let action = agent.action(state)
+            
+            //console.log(action)
+            //sit
+            let next_state = getState(data, t + 1, window_size + 1)
+            let reward = 0
+
+            if(action == 1 && agent.inventory.length === 0) { //buy
+                agent.inventory.push(data[t])
+                // console.log(`Buy: ${data[t]} | Ep: ${e} | D: ${t}`)
+            } else if(action == 2 && agent.inventory.length > 0) { //sell
+                let bought_price = agent.inventory.shift(0)
+                let _profit = data[t] - bought_price
+                let pct = (_profit / bought_price)
+                reward = _profit <= 0 ? 0.01 : pct <= 0.1 ? pct : pct + 1//_.max([_profit, 0])
+                total_profit += isNaN(_profit) ? 0 : _profit
+                total_trades++
+                // console.log(reward)
+                // console.log(`Sell: ${data[t]} | Profit: ${ _profit}`)
+            }
+
+            let done = t == (l - 1)
+
+            if(agent.memory.length > MAX_MEM - 1) {
+                agent.memory.shift()
+            }
+            agent.memory.push([state, action, reward, next_state, done])
+            // console.log(agent.memory.length)
+            state = next_state
+
+            if(done) {
+                console.log('--------------------------------')
+                console.log('Total Profit:', total_profit.toFixed(2), 'Trades:', total_trades)
+                console.log('--------------------------------')
+            }
+
+        }
+        await agent.expReplay(batch_size)
+        if(e % 10 == 10){
+            await agent.model.save('localstorage://models/model-ep' + e)
+        }
+    }
+    console.log('Done')
+}
+train()
